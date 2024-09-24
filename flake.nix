889af74d53db0e98ed371f0b25b8be2bddb0ae36
natsukium <tomoya.otabi@gmail.com>
2024-09-24 23:44:53 +0900

    build: update flake.lock

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           default = pkgs.mkShell {
             buildInputs = with pkgs; [
               nodejs
-              nodePackages.pnpm
+              pnpm
               terraform
             ];
           };
